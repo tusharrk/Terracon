3eed0af85b5519d0b8393c01517ad7625e9cb0fd
tushar kalsara <tushar.kalsara@gmail.com>
2023-09-29 19:54:56 +0530

    tree changes

---
+++ app/src/main/java/com/terracon/survey/utils/AlertUtils.kt
@@ -0,0 +1,11 @@
+package com.terracon.survey.utils
+
+import android.R
+import android.app.Activity
+
+
+object AlertUtils {
+    fun showAlertDialog(activity: Activity, title: String, description: String, type: String) {
+
+    }
+}
