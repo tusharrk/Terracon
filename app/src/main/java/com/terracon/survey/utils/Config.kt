9268bca2b096fc2a4de98b1b0b0e8b42060fcabc
tushar kalsara <tushar.kalsara@gmail.com>
2024-01-02 20:36:08 +0530

    updated url

--- app/src/main/java/com/terracon/survey/utils/Config.kt
+++ app/src/main/java/com/terracon/survey/utils/Config.kt
@@ -1,8 +1,8 @@
 package com.terracon.survey.utils
 
 object Config {
-    const val BASE_URL = "https://terracon.srsdigimind.com"
-    const val FILE_UPLOAD_URL = "https://terracon-cdn.srsdigimind.com/fileupload.php"
+    const val BASE_URL = "https://naturenotes.co.in"
+    const val FILE_UPLOAD_URL = "https://terracon-cdn.naturenotes.co.in/fileupload.php"
     //const val BASE_URL = "http://192.168.52.56:8080"
     const val IMAGE_URL = "https://image.tmdb.org/t/p/original";
 }
